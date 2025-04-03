c06ffada3cea42775600f7f31a0d5fd4684f025c
SemMTM <osama.hendawi@hotmail.com>
2025-04-03 15:39:20 +0100

    Deploy without Json view to test log in

--- SFT_API/settings.py
+++ SFT_API/settings.py
@@ -80,10 +80,6 @@ REST_FRAMEWORK = {
      'rest_framework.permissions.IsAuthenticated',
     ]
 }
-if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRenderer'
-    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
