fe6b102adbe64c4657e1f4b638b0132a0fcfe847
SemMTM <osama.hendawi@hotmail.com>
2025-04-07 12:19:24 +0100

    CHORE: Settings configuration to fix refresh token endpoint

--- SFT_API/settings.py
+++ SFT_API/settings.py
@@ -69,8 +69,9 @@ REST_AUTH = {
     'USE_JWT': True,
     'JWT_AUTH_COOKIE': 'my-app-auth',
     'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
-    'JWT_AUTH_SECURE': not DEBUG,
-    'JWT_AUTH_HTTPONLY': False
+    'JWT_AUTH_SECURE': True,
+    'JWT_AUTH_HTTPONLY': False,
+    'JWT_AUTH_SAMESITE': 'None',
 }
 
 if DEBUG:
