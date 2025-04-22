ef1cacd52567b0efc0c0a5cb07c024659c213f6a
SemMTM <osama.hendawi@hotmail.com>
2025-04-22 11:46:09 +0100

    CHORE: Format income and expen values with no currency symbol, return symbol spearately for calendar view

--- transactions/serializers/calendar_summary.py
+++ transactions/serializers/calendar_summary.py
@@ -4,13 +4,15 @@ from core.utils.currency import get_user_currency_symbol
 
 class CalendarSummarySerializer(serializers.Serializer):
     date = serializers.DateField()
-    formatted_income = serializers.SerializerMethodField()
-    formatted_expenditure = serializers.SerializerMethodField()
+    income = serializers.SerializerMethodField()
+    expenditure = serializers.SerializerMethodField()
+    currency_symbol = serializers.SerializerMethodField()
 
-    def get_formatted_income(self, obj):
-        symbol = get_user_currency_symbol(self.context["request"])
-        return f"{symbol}{obj['income'] / 100:.2f}"
+    def get_income(self, obj):
+        return f"{obj['income'] / 100:.2f}"
 
-    def get_formatted_expenditure(self, obj):
-        symbol = get_user_currency_symbol(self.context["request"])
-        return f"{symbol}{obj['expenditure'] / 100:.2f}"
+    def get_expenditure(self, obj):
+        return f"{obj['expenditure'] / 100:.2f}"
+
+    def get_currency_symbol(self, obj):
+        return get_user_currency_symbol(self.context["request"])
