3e0534a1b579a8dc3809e71b30649b8abbfa3b8d
SemMTM <osama.hendawi@hotmail.com>
2025-05-15 11:19:48 +0100

    TEST: Add unit tests for date_helpers utils file

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -2,6 +2,12 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser, User
 from transactions.models.currency import Currency
 from core.utils.currency import get_currency_symbol, get_user_currency_symbol
+from datetime import datetime
+from django.utils.timezone import make_aware
+from core.utils.date_helpers import (
+    get_user_and_month_range,
+    get_weeks_in_month_clipped
+)
 
 
 class CurrencyUtilsTests(TestCase):
@@ -50,3 +56,68 @@ class CurrencyUtilsTests(TestCase):
         request = self.factory.get('/')
         request.user = self.user
         self.assertEqual(get_user_currency_symbol(request), '¥')
+
+
+class DateHelpersTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='tester', password='testpass')
+
+    def test_get_user_and_month_range_with_valid_param(self):
+        """
+        Returns correct start/end datetimes when a valid ?month=YYYY-MM param is provided.
+        """
+        request = self.factory.get('/?month=2025-09')
+        request.user = self.user
+
+        user, start, end = get_user_and_month_range(request)
+
+        self.assertEqual(user, self.user)
+        self.assertEqual(start, make_aware(datetime(2025, 9, 1, 0, 0)))
+        self.assertEqual(end, make_aware(datetime(2025, 10, 1, 0, 0)))
+
+    def test_get_user_and_month_range_with_invalid_param(self):
+        """
+        Falls back to current month when ?month= param is invalid.
+        """
+        request = self.factory.get('/?month=invalid')
+        request.user = self.user
+
+        user, start, end = get_user_and_month_range(request)
+
+        self.assertEqual(user, self.user)
+        self.assertEqual(start.day, 1)
+        self.assertEqual(start.hour, 0)
+        self.assertTrue(end > start)
+
+    def test_get_user_and_month_range_without_param(self):
+        """
+        Returns current month when no ?month= param is provided.
+        """
+        request = self.factory.get('/')
+        request.user = self.user
+
+        user, start, end = get_user_and_month_range(request)
+
+        self.assertEqual(user, self.user)
+        self.assertEqual(start.day, 1)
+        self.assertEqual(start.hour, 0)
+        self.assertTrue(end > start)
+
+    def test_get_weeks_in_month_clipped_structure(self):
+        """
+        Returns weekly (start, end) tuples clipped to within the month.
+        """
+        request = self.factory.get('/?month=2025-09')
+        request.user = self.user
+
+        user, weeks, start, end = get_weeks_in_month_clipped(request)
+
+        self.assertEqual(user, self.user)
+        self.assertEqual(start, make_aware(datetime(2025, 9, 1, 0, 0)))
+        self.assertEqual(end, make_aware(datetime(2025, 10, 1, 0, 0)))
+        self.assertGreaterEqual(len(weeks), 4)
+
+        for start_week, end_week in weeks:
+            self.assertLessEqual(start_week, end_week)
+            self.assertLessEqual(end_week, end)
